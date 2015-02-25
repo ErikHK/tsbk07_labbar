a9d9c7096637f50f117d7a37b22fb9bac00a556f
Klara <klawi021@student.liu.se>
2015-02-25 13:59:45 +0100

    first

---
+++ lab1/lab1-2.vert
@@ -0,0 +1,9 @@
+#version 130
+
+in  vec3 in_Position;
+uniform mat4 myMatrix;
+
+void main(void)
+{
+	gl_Position = myMatrix*vec4(in_Position, 1.0);
+}
---
+++ lab1/lab1-4.c
@@ -0,0 +1,140 @@
+// Lab 1-1.
+// This is the same as the first simple example in the course book,
+// but with a few error checks.
+// Remember to copy your file to a new on appropriate places during the lab so you keep old results.
+// Note that the files "lab1-1.frag", "lab1-1.vert" are required.
+
+// Should work as is on Linux and Mac. MS Windows needs GLEW or glee.
+// See separate Visual Studio version of my demos.
+#ifdef __APPLE__
+	#include <OpenGL/gl3.h>
+	#include "MicroGlut.h"
+	// Linking hint for Lightweight IDE
+	// uses framework Cocoa
+#endif
+#include "GL_utilities.h"
+#include <math.h>
+
+// Globals
+// Data would normally be read from files
+GLfloat vertices[] =
+{
+	-0.5f,-0.5f,0.0f,
+	-0.5f,0.5f,0.0f,
+	0.5f,-0.5f,0.0f
+};
+
+GLfloat myMatrix[] = {1.0f, 1.0f, 0.0f, 0.5f,
+		0.0f, 4.0f, 0.0f, 0.0f,
+		0.0f, 0.0f,  1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f};
+
+GLfloat myColor[] = {0.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 1.0f,
+		1.0f, 1.0f, 0.0f};
+
+
+// Reference to shader program
+GLuint program;
+
+// vertex array object
+unsigned int vertexArrayObjID;
+
+
+void OnTimer(int value)
+{
+    GLfloat t = (GLfloat)glutGet(GLUT_ELAPSED_TIME)/1000;
+    myMatrix[0] = cos(t);
+    myMatrix[1] = -sin(t);
+    myMatrix[4] = sin(t);
+    myMatrix[5] = cos(t);
+    glutPostRedisplay();
+    glutTimerFunc(20, &OnTimer, value);
+}
+
+
+void init(void)
+{
+	// vertex buffer object, used for uploading the geometry
+	unsigned int vertexBufferObjID;
+	unsigned int vbo_colors;
+	//int attrib_color;
+
+	//init timer
+	glutTimerFunc(20, &OnTimer, 0);
+
+	dumpInfo();
+
+	// GL inits
+	glClearColor(0.2,0.2,0.5,0);
+	glDisable(GL_DEPTH_TEST);
+	printError("GL inits");
+
+	// Load and compile shader
+	program = loadShaders("lab1-4.vert", "lab1-4.frag");
+	printError("init shader");
+	
+	// Upload geometry to the GPU:
+	
+	// Allocate and activate Vertex Array Object
+	glGenVertexArrays(1, &vertexArrayObjID);
+	glBindVertexArray(vertexArrayObjID);
+	// Allocate Vertex Buffer Objects
+	glGenBuffers(1, &vertexBufferObjID);
+
+	glGenBuffers(1, &vbo_colors);
+	
+	// VBO for vertex data
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjID);
+	glBufferData(GL_ARRAY_BUFFER, 9*sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(glGetAttribLocation(program, "in_Position"), 3, GL_FLOAT, GL_FALSE, 0, 0); 
+	glEnableVertexAttribArray(glGetAttribLocation(program, "in_Position"));
+	
+	
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_colors);
+	glBufferData(GL_ARRAY_BUFFER, 9*sizeof(GLfloat), myColor, GL_STATIC_DRAW);
+	glVertexAttribPointer(glGetAttribLocation(program, "myColor"), 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(glGetAttribLocation(program, "myColor"));
+	
+
+	// End of upload of geometry
+	
+	printError("init arrays");
+
+	//upload matrix
+	glUniformMatrix4fv(glGetUniformLocation(program, "myMatrix"), 1, GL_TRUE, myMatrix);
+
+
+	//GLfloat myFloats[] = {0.1f, 0.8f, 0.1f, 1.0f};
+	//GLint myLoc = glGetUniformLocation(program, "myColor");
+	//glUniformMatrix4fv(program, myLoc, 1, myColor);
+	//glUniformMatrix4f(glGetUniformLocation(program, "col"), 1.0f, .5f, .5f, .3f);
+}
+
+
+void display(void)
+{
+	printError("pre display");
+
+	// clear the screen
+	glClear(GL_COLOR_BUFFER_BIT);
+	glUniformMatrix4fv(glGetUniformLocation(program, "myMatrix"), 1, GL_TRUE, myMatrix);
+
+	glBindVertexArray(vertexArrayObjID);	// Select VAO
+	glDrawArrays(GL_TRIANGLES, 0, 3);	// draw object
+	
+	printError("display");
+	
+	glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitContextVersion(3, 2);
+	glutCreateWindow ("GL3 white triangle example");
+	glutDisplayFunc(display); 
+	init ();
+	glutTimerFunc(20, &OnTimer, 0);
+	glutMainLoop();
+}
